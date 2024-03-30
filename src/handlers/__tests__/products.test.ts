ce30d5e2da3e3802ff975f4cb89c4b9b416f2ff9
Walter <walterdcarrizo.19@gmail.com>
2024-03-30 11:26:48 -0300

    Se generó el codigo para testeo del enpoint GET products

--- src/handlers/__tests__/products.test.ts
+++ src/handlers/__tests__/products.test.ts
@@ -2,6 +2,49 @@ import request from 'supertest';
 import server from '../../server';
 
 describe('POST /api/products', () => {
+
+    it('should display validation errors', async() => {
+
+        const response = await request(server).post('/api/products').send({})
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+
+    })
+
+    it('should validate that the price is greater than 0', async() => {
+
+        const response = await request(server).post('/api/products').send({
+            name: "Monitor test 0",
+            price: 0
+        })
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(4)
+
+    })
+
+    it('should validate that the price is a number and greater than 0', async() => {
+
+        const response = await request(server).post('/api/products').send({
+            name: "Monitor test 0",
+            price: "hola"
+        })
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(4)
+
+    })
+
     it('should create a new product', async() => {
         const response = await request(server).post('/api/products').send({
             name: "Mouse Testing",
@@ -10,5 +53,28 @@ describe('POST /api/products', () => {
 
         expect(response.status).toBe(201)
         expect(response.body).toHaveProperty('data')
+
+        expect(response.status).not.toBe(404)
+        expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('errors')
     } )
+})
+
+
+describe('GET /api/products', () => {
+
+    it('should check if api/products url exist', async() => {
+        const response = await request(server).get('/api/products')
+        expect(response.status).not.toBe(404)
+    })
+    it('GET a JSON response with Products', async() => {
+        const response = await request(server).get('/api/products')
+        expect(response.status).toBe(200)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body).toHaveProperty('data')
+        expect(response.body.data).toHaveLength(1)
+
+        expect(response.body).not.toHaveProperty('error')
+    })
+    
 })
